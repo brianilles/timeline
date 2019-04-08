b13b0017d3581e2fb671443b9b83da637d5b1018
Brian Illes <brianrilles@gmail.com>
2019-04-08 06:03:33 -0700

    sass script config

--- client/timeline/src/App/App.js
+++ client/timeline/src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.scss';
+import '../scss/App.scss';
 
 export default function App() {
   return <div className="App">App</div>;
--- client/timeline/src/index.js
+++ client/timeline/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App.js';
-import './index.scss';
+import './scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
